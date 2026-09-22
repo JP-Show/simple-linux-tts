c7cc9b8c25db4810f0a289447e62eca885f48f7a
Andre Luiz <andreluiz.jpofi@gmail.com>
2026-09-22 12:20:10 -0300

    feat(ui): template da ui completa

--- lib/ui.sh
+++ lib/ui.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 yad --title="Simple Linux TTS" \
  --form \
- --field="Texto" ""\
- --field="Language:CB" "English!Portuguese"
+ --field="Texto que será narrado" ""\
+ --field="Language:CB" "English" \
+ --field="Sotaque:CB" "Britanico" \
+ --field="Tonalização:TXT" "" \
+ --field="Ouvir:BTN" "" \
+ --field="Formatos:CB" "MP3!WAV" \
+ --field="Salva:BTN" ""
